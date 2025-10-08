4097f3856d43ff32f3c1fe81a962d2d3e20ce8f0
maram <marami123@hotmail.com>
2025-10-08 15:28:16 +0300

    update all secret and file code

--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -5,8 +5,14 @@ terraform {
       version = ">=4.26.0"
     }
   }
-}
 
+backend "azurerm" {
+    resource_group_name  = "maram-rg"
+    storage_account_name = "maram"
+    container_name       = "maram"
+    key                  = "maram.terraform.tfstate"
+  }
+}
 provider "azurerm" {
   features {}
   subscription_id = "80646857-9142-494b-90c5-32fea6acbc41"
